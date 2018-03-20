ddf7cf95307c0219ba3cd6826c5f433e9d7c57b9
Lucas Moura <lucas.moura@timo.net>
2018-03-20 10:27:58 +0100

    some example pages

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -3,28 +3,52 @@ import scrapy
 
 
 class BooksSpider(scrapy.Spider):
-    name = "books"
+    name = "top-1m"
     allowed_domains = ["books.toscrape.com"]
     start_urls = [
-        'http://books.toscrape.com/',
+        'http://youtube.com/',
+        'http://facebook.com/',
+        'http://baidu.com/',
+        'http://wikipedia.org/',
+        'http://reddit.com/',
+        'http://yahoo.com/',
+        'http://qq.com/',
+        'http://taobao.com/',
+        'http://amazon.com/',
+        'http://tmall.com/',
+        'http://vk.com/',
+        'http://twitter.com/',
+        'http://instagram.com/',
+        'http://sohu.com/',
+        'http://sina.com.cn/',
+        'http://live.com/',
+        'http://jd.com/',
+        'http://360.cn/',
+        'http://weibo.com/',
+        'http://yandex.ru/',
+        'http://google.com.br/',
+        'http://netflix.com/',
+        'http://twitch.tv/',
+        'http://login.tmall.com/',
+        'http://alipay.com/',
+        'http://yahoo.co.jp/',
+        'http://bing.com/',
+        'http://ebay.com/',
+        'http://ok.ru/',
+        'http://hao123.com/',
+        'http://microsoft.com/',
+        'http://t.co/',
+        'http://imgur.com/',
+        'http://wikia.com/',
+        'http://mail.ru/',
+        'http://imdb.com/'
     ]
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
-
-    def parse_book_page(self, response):
         item = {}
-        product = response.css("div.product_main")
-        item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
+        item['url'] = reqeust.url
+        item['title'] = reqeust.css('title::text').extract_first()
+        item['description'] = reqeust.xpath("//meta[@name='description'/text()]").extract_first()
+        item['keywords'] = reqeust.xpath("//meta[@name='keywords'/text()]").extract_first()
         yield item
+
