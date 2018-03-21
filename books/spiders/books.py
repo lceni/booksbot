ad9382f94f2d6184133dca1c38ab61925b4568fb
lucas.ceni <lucas.ceni@gmail.com>
2018-03-21 23:22:50 +0100

    loading the 1m entries from zip file

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -1,54 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import zipfile
+import csv
 
 
 class BooksSpider(scrapy.Spider):
     name = "top-1m"
 
-    start_urls = [
-        'http://youtube.com/',
-        'http://facebook.com/',
-        'http://baidu.com/',
-        'http://wikipedia.org/',
-        'http://reddit.com/',
-        'http://yahoo.com/',
-        'http://qq.com/',
-        'http://taobao.com/',
-        'http://amazon.com/',
-        'http://tmall.com/',
-        'http://vk.com/',
-        'http://twitter.com/',
-        'http://instagram.com/',
-        'http://sohu.com/',
-        'http://sina.com.cn/',
-        'http://live.com/',
-        'http://jd.com/',
-        'http://360.cn/',
-        'http://weibo.com/',
-        'http://yandex.ru/',
-        'http://google.com.br/',
-        'http://netflix.com/',
-        'http://twitch.tv/',
-        'http://login.tmall.com/',
-        'http://alipay.com/',
-        'http://yahoo.co.jp/',
-        'http://bing.com/',
-        'http://ebay.com/',
-        'http://ok.ru/',
-        'http://hao123.com/',
-        'http://microsoft.com/',
-        'http://t.co/',
-        'http://imgur.com/',
-        'http://wikia.com/',
-        'http://mail.ru/',
-        'http://imdb.com/'
-    ]
+    start_urls = []
+
+    def start_requests(self):
+        zipFile = zipfile.Zipfile('../top-1m.csv.zip')
+        csvFile = zipFile.open('top-1m.csv')
+        spamReader = csv.reader(csvFile, delimiter=',')
+        for row in spamReader:
+            yeld scrapy.Request(row[1], self.parse)
+
+
 
     def parse(self, response):
         item = {}
-
-
-
         item['url'] = response.url
         item['title'] = response.css('title::text').extract_first()
         item['description'] = response.xpath("//meta[@name='description']/@content").extract_first()
