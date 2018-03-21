d72ca51708dfb67ffb09b48b7987636267980e1d
lucas.ceni <lucas.ceni@gmail.com>
2018-03-21 22:13:55 +0100

    changing the item model

--- books/items.py
+++ books/items.py
@@ -10,5 +10,8 @@ import scrapy
 
 class BooksItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    description = scrapy.Field()
+    keywords = scrapy.Field()
     pass
